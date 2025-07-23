4b5f09d7a7840f2a47547bf15d0dfd60107d5233
horaciolidity <horacio.dev.sol@gmail.com>
2025-07-23 17:33:39 -0300

    � Ajustes locales previos al pull

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://TU_PROYECTO.supabase.co'
-const supabaseKey = 'TU_PUBLIC_ANON_KEY'
+const supabaseUrl = 'https://hxzvdktzsbuotuibrvik.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4enZka3R6c2J1b3R1aWJydmlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyMTczOTcsImV4cCI6MjA2ODc5MzM5N30.L2G5X27IRBw5As78qEILDH0hRepCWiRD70scliIfCeo'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
